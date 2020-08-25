76f30481b2d468f032b78aefd3c9bb5d15c7019b
Vuducvi1999 <vuducvi20@gmail.com>
2020-08-25 10:42:40 +0700

    fix redux devtool

--- client/src/store.js
+++ client/src/store.js
@@ -9,7 +9,10 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : applyMiddleware(...middleware)
   )
 );
 
